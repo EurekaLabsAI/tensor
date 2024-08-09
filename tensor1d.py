8ec321a7fdff4f85517813bc03528f9fe96d1360
Aniket <aniket@care.ai>
2024-08-09 12:05:05 -0400

    adding tensor.zeros() support with calloc

--- tensor1d.py
+++ tensor1d.py
@@ -19,6 +19,7 @@ typedef struct {
 } Tensor;
 
 Tensor* tensor_empty(int size);
+Tensor* tensor_zeros(int size);
 int logical_to_physical(Tensor *t, int ix);
 float tensor_getitem(Tensor* t, int ix);
 Tensor* tensor_getitem_astensor(Tensor* t, int ix);
@@ -114,6 +115,10 @@ def empty(size):
 
 def arange(size):
     c_tensor = lib.tensor_arange(size)
+    return Tensor(c_tensor=c_tensor)
+
+def zeros(size):
+    c_tensor = lib.tensor_zeros(size)
     return Tensor(c_tensor=c_tensor)
 
 def tensor(data):
